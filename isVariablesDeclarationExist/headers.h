2ec4cb23f15b0699d6380f871b1c3ca8fad90b09
Alex <astrelin2001@gmail.com>
2020-04-19 13:45:27 +0400

    Добавления файла для хранения функций

--- isVariablesDeclarationExist/headers.h
+++ isVariablesDeclarationExist/headers.h
@@ -1,4 +1,11 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
+#include <QCoreApplication>
+#include <QTextStream>
+
+void getAndCheckImputDataFromFiles(QString &pathToCodeFile, QString &pathToVariablesFile,
+                          QStringList &textOfProgramm, QStringList &namesOfVariables);
+
+
 #endif // HEADERS_H
--- isVariablesDeclarationExist/main.cpp
+++ isVariablesDeclarationExist/main.cpp
@@ -1,4 +1,4 @@
-#include "headers.h"
+#include "functions.cpp"
 
 int main(int argc, char *argv[])
 {
@@ -40,22 +40,3 @@ int main(int argc, char *argv[])
 
 }
 
-/**
-    \brief Чтение данных из командной строки, их проверка, чтение из файлов и проверка данных из файла переменных
-    \param[in] pathToCodeFile - путь к файлу с текстом программы на языке Си
-    \param[in] pathToVariablesFile - путь к файлу с переменными
-    \param[out] textOfProgramm - текст программы на языке Си
-    \param[out] namesOfVariables - имена переменных, которые нужно проверить
-    \throw Если файл с кодом не .txt
-    \throw Если файл с переменным не .txt
-*/
-void getAndCheckImputDataFromFiles(QString &pathToCodeFile, QString &pathToVariablesFile,
-                          QStringList &textOfProgramm, QStringList &namesOfVariables){
-    // Проверка расширения обоих конечных файлов указанных в пути
-
-    // Чтение из файлов
-
-    // Удаление пустых строчек в коде программы
-
-    // Проверка имен переменных
-}
